3aa702e6e4c28110b679f4fbfbb04f8e99f8a580
Lev <l.e.chechulin@gmail.com>
2022-07-11 23:21:55 +0200

    sm change

--- app/db_conn.py
+++ app/db_conn.py
@@ -1,3 +1,4 @@
+import os 
 import psycopg2
 
 def get_db_connection():
--- app/init_db.py
+++ app/init_db.py
@@ -3,17 +3,17 @@ import psycopg2
 
 conn = psycopg2.connect(
         host="localhost",
-        database="flask_db",
-        user=os.environ['DB_USER'],
+        #database="flask_db",
+        user=os.environ['DB_USERNAME'],
         password=os.environ['DB_PASSWORD'])
 conn.set_session(autocommit = True)
 
 cur = conn.cursor()
 
-cur.execute("CREATE DATABASE Links")
+cur.execute("CREATE DATABASE IF NOT EXISTS Links")
 
-conn.execute('DROP TABLE IF EXISTS links;')
-conn.execute('CREATE TABLE links ('
+cur.execute('DROP TABLE IF EXISTS links;')
+cur.execute('CREATE TABLE links ('
     'id INTEGER PRIMARY KEY AUTOINCREMENT,'
     'url TEXT NOT NULL'
     'shortlink TEXT NOT NULL);'
