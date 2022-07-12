7bf56b4382560cf644d807ecdfe54bcf879e88c8
Lev <l.e.chechulin@gmail.com>
2022-07-12 03:06:14 +0200

    fire burns

--- urlshortener.py
+++ urlshortener.py
@@ -1,8 +1,5 @@
-from flask import Flask, render_template, redirect, request
-
-
-app = Flask(__name__)
+from app import app 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host = "127.0.0.2", debug=True)
